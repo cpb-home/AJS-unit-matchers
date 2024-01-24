83fba5efdd9f175c977b87369140240e9fd3a2fa
cpb-home <cpb@bizb.ru>
2024-01-24 19:21:06 +0300

    moved variable

--- src/js/__tests__/heroesSort.test.js
+++ src/js/__tests__/heroesSort.test.js
@@ -1,13 +1,12 @@
 import { sortHeroes } from '../heroesSort';
 import { heroes } from '../app';
 
-const mustbe = [
-  {name: 'маг', health: 100},
-  {name: 'лучник', health: 80},
-  {name: 'мечник', health: 10},
-];
-
 test('testing heroes sorting', () => {
+  const mustbe = [
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+    {name: 'мечник', health: 10},
+  ];
   const result = sortHeroes(heroes);
   expect(result).toEqual(mustbe);
 });
